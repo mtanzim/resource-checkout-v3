27e58ab547fabf30dc0cfebcc234f6a5ca39c8a9
Tanzim Mokammel <mtanzim@gmail.com>
2023-07-03 15:14:45 -0400

    very basic setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import { redirect } from 'next/navigation'
+"use client";
+import Link from "next/link";
 
 export default function Home() {
-  return redirect('/resource-groups')
+  return (
+    <main>
+      <h1>Welcome to the Resource Checkout app!!</h1>
+      <Link href="/resource-groups">
+        <button className="btn btn-primary">Get started</button>
+      </Link>
+    </main>
+  );
 }
